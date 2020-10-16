9c6666d79f9d5f166df34583cbaf2bd2dc71b864
Alwin Blok <alwinb@users.noreply.github.com>
2020-10-16 13:48:54 +0200

    Lexer - Refactor the labelling code

--- test/lexer-test.js
+++ test/lexer-test.js
@@ -4,38 +4,51 @@ const { stateNames, Lexer, tags, chunks, printState } = require ('../lib')
 // Test
 // ====
 
+
 function pr (input) {
   if (input.length < 100)
     log (input, '\n'+input.split('').map(_=>'=').join(''))
   let stream = chunks (input)
-  log (printState (stream.state))
+  log (printState (stream.state), '\n\n')
   for (let [t, v] of stream) {
-    log ([stateNames[t], v], '\n', printState(stream.state))
+    log ([stateNames[t], v])
+    log (printState (stream.state), '\n\n')
   }
 
-  log ('\ntokens\n========')
+  log (input, '\n\nTokens\n========')
   stream = tags (input)
+  log (printState (stream.state), '\n\n')
   for (let x of stream) {
-    log ([stateNames[x[0]], x[1]])
-    //log (x)
-    //log (printState (stream.state))
+    log (x)
+    log (printState (stream.state), '\n\n')
   }
 }
 
 //*
 var sample =`
 <a href = foo >test<!-- foo --!></bar>`
+
+var sample = `data<!-->data`
+var sample = `data<!---!>comment`
 pr (sample)
-//*/
+
+
+
+
+/*/
 
 // Multiple writes
 //*
+log ('\n\nMultiple writes\n========')
+
 var l = new Lexer
 l.write ('<span hr')
 l.write ('n href="foo">')
 l.end ('<!--comment >')
 
 log (l.state)
-for (let [t,v] of l.read ())
-  log ([stateNames[t], v], l.state)
+for (let [t,v] of l.read ()) {
+  log ([stateNames[t], v])
+  log (printState (l.state), _printState (l.state), '\n\n')
+}
 //*/
