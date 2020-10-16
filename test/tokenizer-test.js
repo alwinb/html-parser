a00b2492f0c08aa5b008dc2c832c779efb166870
Alwin Blok <alwinb@users.noreply.github.com>
2020-10-16 21:59:19 +0200

    Let the TokenBuilder generate FormatStart/ FormatEndTags

--- test/tokenizer-test.js
+++ test/tokenizer-test.js
@@ -12,7 +12,7 @@ function tokenize (str) {
   return b.end().readAll ()
 }
 
-var sample = '<!Doctype html><h1>header</h1><span att= a att2= "10 &amp 20">Foo bar <!-- bee baz buz --> </span>'
+var sample = '<!Doctype html><h1>header</h1><span att= a att2= "10 &amp 20">Foo </b>bar <!-- bee baz buz --> </span>'
 //var sample = '<h1>Test</h1><! foo bar bax ><li> and data'
 
 log (util.inspect(tokenize (sample), { depth:100}))
