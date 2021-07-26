3162d9d2271c1d33c41263abebbef6dc38f3129d
Alwin Blok <alwinb@users.noreply.github.com>
2021-07-26 15:58:05 +0200

    Use bitfields for tokenTypes as well

--- test/tokenizer-test.js
+++ test/tokenizer-test.js
@@ -9,16 +9,17 @@ function tokenize (str) {
   for (let x of l.write (str) .end () .read ()) {
     b.write (x)
   }
-  return b.end().readAll ()
+  return b.end () .readAll ()
 }
 
 var sample = '<!Doctype html><h1>header</h1><span att= a att2= "10 &amp 20">Foo </b>bar <!-- bee baz buz --> </span>'
 //var sample = '<h1>Test</h1><! foo bar bax ><li> and data'
 var sample = `<!DOCTYPE html x>text`
-var sample = `<s o=& t>`
 var sample = `<!->`
 var sample = `<fo><br>bar</br><i foo=1>Italic</i>`
 var sample = `<h a='&COPY'b/d=c>`
 var sample = `foo bar`
+var sample = `<s o=& t>`
+var sample = `</s o=& t>`
 
 log (util.inspect(tokenize (sample), { depth:100 }))
