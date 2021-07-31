23575ff93d7aaf309893cadfd8f07643353634e6
Alwin Blok <alwinb@users.noreply.github.com>
2021-07-31 10:26:53 +0200

    Redirection into the head

--- test/treebuilder.new.test.js
+++ test/treebuilder.new.test.js
@@ -42,74 +42,11 @@ setTimeout (() => {
 var sample1 = '<div>foo'
 var sample1 = '<table><caption><select>foo<table><select>bar'
 // var sample1 = '<table><caption><select>foo<select>bar'
+ // var sample1 = '<!doctype html><p>Test<h1>Head1<table>foo<div></h2>Text'
 var sample1 = '<head></head>After head</head>Foo'
-var sample1 = '<select><option>foo<option>'
-
-// Result
-
-
-const S = START, X = END, L = LEAF
-
-var sample = [
-  [S, 'title'],
-  // [S, 'script'], // FIXME should not allow elements
-  // [S, 'div'],
-  // [E, 'br'],
-  // [S, 'br'],
-  // [S, 'image'],
-  // [X, 'p'],
-  // [X, 'p'],
-  // [S, 'select'],
-  // [S, 'select'],
-  // [S, 'foo'],
-  [S, 'table'],
-  [S, 'bar'],
-  [S, 'td'],
-  [L, 'foo'],
-  [S, 'td'],
-  [X, 'td'],
-  [L, 'bar'],
-  [L, ' '],
-  [L, 'bee'],
-  [X, 'br'],
-  [L, 'buzz'],
-]
-
-// good example!
-//*
-var sample = [
-  [S, 'table'],
-  [S, 'p'],
-  [S, 'td'],
-  [L, 'foo'],
-  [X, 'td'],
-  [X, 'p'],
-  [S, 'td'],
-  [S, 'table'],
-  [L, 'bar'],
-  [X, 'p'],
-]
-
-var sample_ = [
-  [S, 'table'],
-  [L, 'foo'],
-  [S, 'table'],
-  [L, 'bar'],
-]
-
-var sample = [
-  [S, 'p'],
-  [L, 'foo'],
-  [S, 'table'],
-  [L, 'foo'],
-  [S, 'input'],
-  [S, 'input', { type:'HiDDen' }],
-  [S, 'tr', { class:'test' }],
-  [S, 'input', { type:'hiDDen' }],
-  [S, 'input', { type:'hiDDen' }],
-  [L, 'test'], // FIXME doesn't get added to the target?
-  [S, 'p'],
-  [X, 'body'],
-  [L, 'foo'],
-]
-//*/
+var sample1 = '<html><head> <link> </head> <link>'
+var sample1 = '<html><frameset>'
+var sample1 = '</head> <p>'
+// var sample1 = '<math><annotation-xml encoding=TeXt/Html><p><p>'
+// var sample1 = '<svg><desc>test<div>'
+// var sample1 = '<font face>foo'
