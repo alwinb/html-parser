e4836bedcdead199f14a03a0f5574e0790678438
Alwin Blok <alwinb@users.noreply.github.com>
2021-08-01 03:50:33 +0200

    More and more tidy!

--- test/director-test.js
+++ test/director-test.js
@@ -20,41 +20,36 @@ function Parser (options) {
   }
 }
 
-// Test
-// ====
-//*
-
-setTimeout (() => {
-  const verbose = true
-  // var d = new Director ()
-  // d.batchWrite (sample)
-  // var builder = d.builder
-  var p = new Parser ({ verbose })
-  var doc = p.parse (sample1) 
-  log (util.inspect (doc, {depth:Infinity}))
-})
-
-
 // Samples
 // -------
 
 
-var sample1 = '<div>foo'
-var sample1 = '<table><caption><select>foo<table><select>bar'
-// var sample1 = '<table><caption><select>foo<select>bar'
- // var sample1 = '<!doctype html><p>Test<h1>Head1<table>foo<div></h2>Text'
-var sample1 = '<head></head>After head</head>Foo'
-var sample1 = '<html><head> <link> </head> <link>'
-var sample1 = '<html><frameset>'
-var sample1 = '</head> <p>'
-var sample1 = '<math><annotation-xml encoding=TeXt/Html><p><p>'
-var sample1 = '<svg><desc>test<div>'
-var sample1 = '<font face>foo'
-var sample1 = '<!doctype>foo'
-var sample1 = '<table> g'
-var sample1 = '</body><title>X</title>'
-var sample1 = '<param><frameset>'
-var sample1 = '<svg><foo/><bar>'
+var sample = '<div>foo'
+var sample = '<table><caption><select>foo<table><select>bar'
+// var sample = '<table><caption><select>foo<select>bar'
+// var sample = '<!doctype html><p>Test<h1>Head1<table>foo<div></h2>Text'
+var sample = '<head></head>After head</head>Foo'
+var sample = '<html><head> <link> </head> <link>'
+var sample = '<html><frameset>'
+var sample = '</head> <p>'
+var sample = '<math><annotation-xml encoding=TeXt/Html><p><p>'
+var sample = '<svg><desc>test<div>'
+var sample = '<font face>foo'
+var sample = '<!doctype>foo'
+var sample = '<table> g'
+var sample = '</body><title>X</title>'
+var sample = '<param><frameset>'
+var sample = '<svg><foo/><bar>'
+var sample = '<head></head><title>X</title>'
 
 
-/* Compute the frameset OK flags **/
+// Test
+// ====
+
+const verbose = true
+// var d = new Director ()
+// d.batchWrite (sample)
+// var builder = d.builder
+var p = new Parser ({ verbose })
+var doc = p.parse (sample) 
+log (util.inspect (doc, {depth:Infinity}))
